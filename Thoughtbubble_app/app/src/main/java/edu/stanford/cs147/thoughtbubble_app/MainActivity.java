7f34009938433bc50e7cf2bf3a8f027b0f184050
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-11-17 16:32:50 -0800

    intent should be working

--- Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/MainActivity.java
+++ Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.stanford.cs147.thoughtbubble_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ProfilePage(View view) {
+        Intent intent = new Intent(this, ProfilePage.class);
+        startActivity(intent);
+    }
+
+    public void AnswerPage(View view) {
+        Intent intent = new Intent(this, Answer.class);
+        startActivity(intent);
+    }
+
+    public void AskPage(View view) {
+        Intent intent = new Intent(this, Ask.class);
+        startActivity(intent);
+    }
+
+    public void DiscoverPage(View view) {
+        // controlled by the main page
     }
 }
