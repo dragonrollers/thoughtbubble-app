d3dc08f0f3cdaa6634961346113168e333911228
Grace Hong <ghong97@stanford.edu>
2017-11-22 16:20:05 -0800

    Add list adapter

--- Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/AskSelectActivity.java
+++ Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/AskSelectActivity.java
@@ -4,16 +4,61 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class AskSelectActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class AskSelectActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ArrayList<String> myArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_select);
+
+        //TODO: actually replace this with the real list
+        myArray = new ArrayList<>();
+        myArray.add("Grace");
+        myArray.add("Po");
+        myArray.add("Bonnie");
+        myArray.add("Jenny");
+
+        //TODO: will have to use a different layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, myArray
+        );
+
+        ListView list = (ListView) findViewById(R.id.ask_friends_list);
+        list.setOnItemClickListener(
+                this
+//                new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                        String clickedName = myArray.get(position);
+//                        Intent intent = new Intent();
+//                        intent.putExtra("sendTo", clickedName);
+//                        setResult(RESULT_OK, intent);
+//                        finish();
+//                    }
+//                }
+        );
+        list.setAdapter(adapter);
     }
 
-    public void SendDummyResult(View view){
+    @Override
+    public void onItemClick(AdapterView<?> list, View row, int index, long rowID) {
+        String clickedName = myArray.get(index);
+        Intent intent = new Intent();
+        intent.putExtra("sendTo", clickedName);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    //TODO: remove
+    public void SendDummyResult(View view) {
         Intent intent = new Intent();
         intent.putExtra("sendTo", "Dummy");
         setResult(RESULT_OK, intent);
