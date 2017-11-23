171ed0ab755a0cea2a85e6592d23fcdf4f91f05a
Grace Hong <ghong97@stanford.edu>
2017-11-22 18:23:25 -0800

    Comment askSelect activity

--- Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/AskSelectActivity.java
+++ Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/AskSelectActivity.java
@@ -20,27 +20,40 @@ public class AskSelectActivity extends AppCompatActivity implements AdapterView.
         setContentView(R.layout.activity_ask_select);
 
         //TODO: actually replace this with the real list
+        //Tester list
         myArray = new ArrayList<>();
         myArray.add("Grace");
         myArray.add("Po");
         myArray.add("Bonnie");
         myArray.add("Jenny");
 
-        //TODO: will have to use a different layout
+        //TODO: will have to use a different layout in order to accomodate more than one string
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1, myArray
         );
 
         ListView list = (ListView) findViewById(R.id.ask_friends_list);
-        list.setOnItemClickListener(this);
-        list.setAdapter(adapter);
+        list.setOnItemClickListener(this); //connects onItemClick function to clicking list items
+        list.setAdapter(adapter); //displays the list in the xml
     }
 
+    /**
+     * ON CLICK METHOD: onItemClick
+     * ----------------------------
+     * This function is called whenever an item in the list is clicked. It retrieves the name
+     * of whoever was clicked and returns this to the previous ask question intent
+     *
+     * @param list
+     * @param row
+     * @param index use this to figure out which element of the array to pull
+     * @param rowID
+     */
     @Override
     public void onItemClick(AdapterView<?> list, View row, int index, long rowID) {
         String clickedName = myArray.get(index);
         Intent intent = new Intent();
         intent.putExtra("sendTo", clickedName);
+        //TODO: should probably also send back the ID for good measure
         setResult(RESULT_OK, intent);
         finish();
     }
