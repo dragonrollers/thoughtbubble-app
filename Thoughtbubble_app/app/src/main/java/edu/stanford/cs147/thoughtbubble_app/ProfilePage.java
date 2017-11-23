1b778988d837cfa7bf384ce4c1b6c0fa537c88f5
Grace Hong <ghong97@stanford.edu>
2017-11-22 18:53:22 -0800

    Dynamically load profile info from activity

--- Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/ProfilePage.java
+++ Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/ProfilePage.java
@@ -2,6 +2,7 @@ package edu.stanford.cs147.thoughtbubble_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfilePage extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class ProfilePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        loadProfileText();
+    }
+    
+    private void loadProfileText() {
+        //TODO: load these from databases using the id of the user
+        String name = "Sample Name";
+        String topics = "Sample Interest 1, Sample Interest 2, Sample Interest 3";
+
+        TextView nameField = (TextView) findViewById(R.id.profile_name);
+        nameField.setText(name);
+
+        TextView topicsField = (TextView) findViewById(R.id.profile_topics);
+        topicsField.setText(topics);
     }
 }
