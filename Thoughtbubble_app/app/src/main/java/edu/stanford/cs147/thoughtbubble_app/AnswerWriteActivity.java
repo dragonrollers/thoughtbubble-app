1243d17c69d82fdc74f3827760baa3e9f60b72e7
Grace Hong <ghong97@stanford.edu>
2017-11-22 17:34:54 -0800

    Add question text

--- Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/AnswerWriteActivity.java
+++ Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/AnswerWriteActivity.java
@@ -3,6 +3,7 @@ package edu.stanford.cs147.thoughtbubble_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AnswerWriteActivity extends AppCompatActivity {
 
@@ -18,5 +19,13 @@ public class AnswerWriteActivity extends AppCompatActivity {
             //ERROR: the activity was not launched with an intent. Terminate
             finish();
         }
+
+        loadQuestionText(questionID);
+    }
+
+    private void loadQuestionText(int questionID) {
+        TextView questionText = (TextView) findViewById(R.id.answerWrite_question_text);
+        //TODO: load questions somehow
+        questionText.setText("Question Dummy Text");
     }
 }
