67bafedb5542b296063ddae034028a541346e546
Bonnie Nortz <bnortz@cs.stanford.edu>
2017-12-06 17:29:35 -0800

    Silly AndroidStudio didn't import some things it had to so this commit fixes that?

--- Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/DatabaseHelper.java
+++ Thoughtbubble_app/app/src/main/java/edu/stanford/cs147/thoughtbubble_app/DatabaseHelper.java
@@ -2,9 +2,11 @@ package edu.stanford.cs147.thoughtbubble_app;
 
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
